a13246a767cf40c6823bbdab2d2996956f7b2178
Tofik Ali <tofik.ali7@gmail.com>
2026-02-20 23:14:29 +0530

    Remove all files (remote reset)

--- Experiments/Experiment-03/scripts/09_table.py
+++
@@ -1,5 +0,0 @@
-# Print multiplication table
-n = int(input("Enter a number: "))
-
-for i in range(1, 11):
-    print(f"{n} * {i} = {n * i}")
--- Experiments/Experiment-06/scripts/06_lambda_max_min.py
+++
@@ -1,4 +0,0 @@
-# Lambda for tuple of max and min
-values = list(map(int, input("Enter numbers: ").split()))
-max_min = lambda lst: (max(lst), min(lst))
-print("(max, min):", max_min(values))
